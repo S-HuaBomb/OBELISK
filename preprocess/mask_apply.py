57eaee2307de9c4e87e7f1b68670531e45d3b1d3
skrBang <s_huabang@163.com>
2021-12-23 21:42:28 +0800

    rm some bad MR examples

--- preprocess/mask_apply.py
+++ preprocess/mask_apply.py
@@ -4,13 +4,17 @@ import nibabel as nib
 import pathlib
 
 
-def rename_(image_dir, label_dir):
+def rename_(image_dir, label_dir, img_type="CT"):
     for idx, image_name in enumerate(os.listdir(image_dir)):
         # image_number = int(image_name.split("_")[0][3:])
 
         label_name = image_name.replace("img", "seg")
-        new_image_name = f"img{idx+1}_bcv_CT.nii.gz"
-        new_label_name = f"seg{idx+1}_bcv_CT.nii.gz"
+        if img_type == "CT":
+            new_image_name = f"img{idx+1}_bcv_CT.nii.gz"
+            new_label_name = f"seg{idx+1}_bcv_CT.nii.gz"
+        elif img_type == "MR":
+            new_image_name = f"img{idx+1}_chaos_MR.nii.gz"
+            new_label_name = f"seg{idx+1}_chaos_MR.nii.gz"
 
         src_image_path = os.path.join(image_dir, image_name)
         dst_iamge_path = os.path.join(image_dir, new_image_name)
@@ -41,19 +45,20 @@ def apply_mask(image_dir, mask_dir, out_dir, img_type="CT"):
 
         masked_img = img * mask
         if img_type == "CT":
+            # CT 的 -1024 是黑色，MR 的 0 是黑色
             masked_img[masked_img == 0.] = -1024.
 
         # masked_img = (masked_img - masked_img.mean()) / masked_img.std()  # mean-std scale
 
         save_path = os.path.join(out_dir, image_name)
         nib.save(nib.Nifti1Image(masked_img, affine=img_affine), save_path)
-        print(f"Masked image saved to {save_path}")
+        print(f"Masked {image_name} saved to {save_path}")
 
 
 if __name__ == "__main__":
     image_dir = r"D:\paper_time\MICCAI\Learn2Reg2021\datasets\task1_Abdominal_MRI_CT_intra_patient\auxiliary\L2R_Task1_MR\MRIs"
     mask_dir = r"D:\paper_time\MICCAI\Learn2Reg2021\datasets\task1_Abdominal_MRI_CT_intra_patient\L2R_Task1_ROIs\L2R_Task1_MR"
-    out_dir = "./MICCAI2021/auxiliary/L2R_Task1_MR/MRIs/"
+    out_dir = r"./MICCAI2021/auxiliary/L2R_Task1_MR/MRIs/"
     label_dir = r"./MICCAI2021/auxiliary/L2R_Task1_MR/labels/"
     # apply_mask(image_dir, mask_dir, out_dir, img_type="MRI")
-    rename_(image_dir=out_dir, label_dir=label_dir)
+    # rename_(image_dir=out_dir, label_dir=label_dir, img_type='MR')
